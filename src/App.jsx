417e89fb9bfed55ac82a39db7043bee8c3680e40
Adarsh Negi <adarshn8487@gmail.com>
2023-08-26 21:12:10 +0530

    Added search field for image

--- src/App.jsx
+++ src/App.jsx
@@ -10,31 +10,13 @@ const App = () => {
   const inputRef = useRef(null);
   
   const [myData, setMyData] = useState([]);
-  const [message, setMessage] = useState('');
   const [isError, setIsError] = useState("");
 
-  const handleChange = event => {
-    let msg=event.target.value
-    setMessage(msg);
-    console.log('value is:',msg);
-  };
 
+  function handleClick() {
+    getApiData(`${url}`+inputRef.current.value);
+  }
 
-  // useEffect(() => {
-    
-  //   axios.get(
-  //     url,
-  //     {headers: {
-  //         "Authorization" : api
-  //       }
-  //     }
-  //   )
-  //     .then(  (response) => { 
-  //         setMyData(response.data.photos)
-  //         console.log(response.data.photos.src) 
-  //       })
-  //     .catch((error) => setIsError(error.message));
-  // }, []);
 
   const getApiData = async(urlo) =>{
     try {
@@ -52,49 +34,51 @@ const App = () => {
 
   }
 
-  useEffect(()=> {
-    getApiData(`${url}`+inputRef.current.value);
-
-  },[]);
-
-
 
   return (
     <>
       <h1>Axios Tutorial</h1>
       {isError !== "" && <h2>{isError}</h2>}
-      {/* <input
-        type="text"
-        id="message"
-        name="message"
-        onChange={handleChange}
-        value={message}
-      />
 
-      <h2>Message: {message}</h2> */}
-
-        <input
+        {/* <input
         ref={inputRef}
         type="text"
         id="message"
         name="message"
-      />
-
-      <button>Log message</button>
+      /> */}
       
-        <div className="grid">{
-        myData.slice(0,1).map((post) => {
-          const { id ,width, height, url ,src} = post;
-          return (
-            <div key={id} className="card">
-              <h2>{width}</h2>
-              <p>{height}</p>
-              <p>{url}</p>
-              <p>{src.medium}</p>
-            </div>
-          );
-        })}
-        </div>
+      {/* <button onClick={handleClick}>Find</button> */}
+      <div class="input-group">
+        <input ref={inputRef}
+          type="text" id="message"
+          name="message" 
+          class="form-control rounded"
+          placeholder="Search" aria-label="Search"
+          aria-describedby="search-addon" />
+
+        <button type="button" class="btn btn-dark" onClick={handleClick}>Find</button>
+      </div>
+ 
+            <div className="row">
+            {
+             
+                  myData.slice(0,1).map((post) => {
+                  const { id ,width, height, url ,src} = post;
+                  return (
+                    <div key={id}>
+                        {/* <h2>{width}</h2>
+                        <p>{height}</p>
+                        <p>{url}</p>
+                        <p>{src.medium}</p> */}
+                        <img src={src.medium} className="searched-img"/>
+                    </div>
+                  );
+                  })
+               
+          }
+          </div>
+   
+        
         
     
     </>
